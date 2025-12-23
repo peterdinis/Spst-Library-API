7ce430a4168dad4bc07a4ee3e0889c5cec186dd3
peterdinis <petter1@pobox.sk>
2025-12-23 10:35:29 +0100

    feat: Update NotificationsService

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -18,11 +18,7 @@ builder.Services.AddCors(options =>
         policy =>
         {
             policy.WithOrigins(
-                    "http://localhost:3000",
-                    "https://localhost:3000",
-                    "http://localhost:5173",
-                    "https://localhost:5173",
-                    "http://localhost:4200")
+                    "http://localhost:3000")
                 .AllowAnyMethod()
                 .AllowAnyHeader();
         });
