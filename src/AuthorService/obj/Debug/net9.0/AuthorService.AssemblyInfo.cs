88fd2ea69e8aef6a150df88dece214502e4ef51e
peterdinis <petter1@pobox.sk>
2025-12-27 19:52:30 +0100

    FOO

--- src/AuthorService/obj/Debug/net9.0/AuthorService.AssemblyInfo.cs
+++ src/AuthorService/obj/Debug/net9.0/AuthorService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthorService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+576d78561ab4e52b26094135e367a45f825a831a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c9df6edd9e471f8babf249a4ad832328f8f8572")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthorService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthorService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
